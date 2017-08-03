a873bbd74ad59be461e1f9e566f119d8abf73ff9
wuhang@X240 <wuhangdeyouxiang@126.com>
2017-08-03 15:34:40 +0800

    idea配置备份整理与eureka-Server代码：
    1. idea配置放到idea-settings并备份到2017-8-3
    2. 增加eureka-server

---
+++ spring-cloud-example/eureka-server/src/main/java/com/cmsz/eureka/server/App.java
@@ -0,0 +1,17 @@
+package com.cmsz.eureka.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(App.class).web(true).run(args);
+	}
+}
